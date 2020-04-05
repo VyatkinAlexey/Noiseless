ff91328e1ff5bc117330817d1312f558a4514739
vitekspeedcuber <shipitsin@phystech.edu>
2020-04-05 17:04:02 +0300

    some minor fixes

--- scripts/erasing_noise_overlay.py
+++ scripts/erasing_noise_overlay.py
@@ -38,7 +38,7 @@ def arguments_parsing(argv):
             noise_type = int(arg)
     
     return path_to_clean_images, path_to_corrupted_images, noise_type
-
+    
 class _Random_Erasing(object):
     """Randomly selects a rectangle region in an image and erases its pixels.
        
@@ -50,16 +50,19 @@ class _Random_Erasing(object):
               If a str of 'random', erasing each pixel with random values.
        inplace: boolean to make this transform inplace. Default set to False.
     """
-    def __init__(self, p=1, scale=(0., 0.7), ratio=(0.7, 0.5), value='random', inplace=False):
+    def __init__(self, p=1., scale=(0.01, 0.012), ratio=(0.5, 0.6), value=0, inplace=False):
         self.p = p
         self.scale = scale
         self.ratio = ratio
         self.value = value 
         self.inplace = inplace
+        self.num_repetitions = 8
               
     def __call__(self, image):
         tensor = ToTensor()(image)
-        return ToPILImage()(RandomErasing(self.p, self.scale, self.ratio, self.value, self.inplace)(tensor))
+        for repetition in range(self.num_repetitions):
+            tensor = RandomErasing(self.p, self.scale, self.ratio, self.value, self.inplace)(tensor)
+        return ToPILImage()(tensor)
     
 def noise_overlay(path_to_clean_images, path_to_corrupted_images, noise_type, noise_level):
     images_names = os.listdir(path_to_clean_images)
