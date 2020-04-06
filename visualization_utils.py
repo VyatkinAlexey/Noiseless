6f9c8f96df73200298e3b34ae00c654b702011a4
suerte412 <rubalex14@gmail.com>
2020-04-06 05:57:39 +0300

    update latent vector visualization notebook

--- visualization_utils.py
+++ visualization_utils.py
@@ -8,11 +8,12 @@ def BarhPlot(latent_vector, full_savepath):
     """Plot horizontal bars for absolute values of each latent vector components"""
     plt.figure(figsize=(10,20))
     plt.barh(range(latent_vector.shape[0]), latent_vector)
-    plt.xlabel("Value")
-    plt.ylabel("Component number")
-    plt.title("Latent vector components values")
+    plt.xlabel("Value", fontsize=16)
+    plt.ylabel("Component number", fontsize=16)
+    # plt.title("Latent vector components values")
     plt.ylim(0, latent_vector.shape[0])
-    plt.savefig(full_savepath)
+    plt.xticks(fontsize=16)
+    plt.savefig(full_savepath, bbox_inches='tight')
     plt.close()
 
 def CountPlot(latent_vector, savepath, eps=1e-2):
